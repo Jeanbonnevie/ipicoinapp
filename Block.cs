718963148ef99c4853e62e7846670ddbebf02c0b
Vladimir Chantitch <96313033+VladimirChantitch@users.noreply.github.com>
2024-05-02 16:57:04 +0200

    id fix

--- Block.cs
+++ Block.cs
@@ -8,8 +8,8 @@ public class Block
     {
         Block block = JsonConvert.DeserializeObject<Block>(BlockJSON);
 
-        if (block.Id.Length != 256) throw new Exception("Id error");
-        if (block.PrevId.Length != 256) throw new Exception("previd error");
+        if (block.id.Length != 256) throw new Exception("Id error " + block.id.Length);
+        if (block.prevId.Length != 256) throw new Exception("previd error");
         if (block.nonce.Length != 256) throw new Exception("nonce error");
         if (block.height < 0 || block.height >= long.MaxValue) throw new Exception("height error");
         if (block.timestamp > DateTime.Now) throw new Exception("timestamp error");
@@ -17,9 +17,9 @@ public class Block
         return block;
     }
 
-    public string Id;
-    public string PrevId;
-    public string[] Data;
+    public string id;
+    public string prevId;
+    public string[] data;
     public string nonce;
     public long height;
     public DateTime timestamp;
