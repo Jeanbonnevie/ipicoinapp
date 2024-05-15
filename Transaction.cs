a18a85f3a160cd314354d2ed0ddc947797547ab4
MaxenceVanel <maxence.vanel@ipilyon.net>
2024-05-15 12:14:29 +0200

    Methode NewTransaction

--- Transaction.cs
+++ Transaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,11 @@ public class Transaction
     {
         Transaction transaction = JsonConvert.DeserializeObject<Transaction>(TransactionJSON);
 
-        if (transaction.addrSender.Length != 256) throw new Exception();
-        if (transaction.addrRcpt.Length != 256) throw new Exception();
-        if (transaction.amount != 256) throw new Exception();
-        if (transaction.Sig.Length != 256) throw new Exception();
-        if (transaction.timestamp != 256) throw new Exception();
+        if (transaction.addrSender.Length != 64) throw new Exception();
+        if (transaction.addrRcpt.Length != 64) throw new Exception();
+        if (transaction.amount != 64) throw new Exception();
+        if (transaction.Sig.Length != 64) throw new Exception();
+        if (transaction.timestamp != 64) throw new Exception();
 
         return transaction;
     }
@@ -26,4 +27,24 @@ public class Transaction
     public float amount;
     public long timestamp;
     public string Sig;
+
+    public static Transaction CreateNewTransaction(string sender, string recipient, float amount, long timestamp, string signature)
+    {
+        if (string.IsNullOrEmpty(sender) || string.IsNullOrEmpty(recipient))
+        {
+            throw new ArgumentException("Sender and Recipient cannot be null or empty.");
+        }
+
+        Transaction transaction = new Transaction
+        {
+            addrSender = sender,
+            addrRcpt = recipient,
+            amount = amount,
+            timestamp = timestamp,
+            Sig = signature
+        };
+
+        return transaction;
+    }
 }
+
